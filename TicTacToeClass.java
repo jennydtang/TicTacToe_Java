2904fe0b6d44c441e1f9b48699769b1b3b00a938
Jenny Tang <jennydtang@gmail.com>
2020-06-03 19:08:51 -0400

    Separate Main into Runner file

--- TicTacToeClass.java
+++ TicTacToeClass.java
@@ -3,78 +3,24 @@ package TicTacToe;
 import java.util.Scanner;
 
 public class TicTacToeClass {
-    public static final int EMPTY = 0;
-    private int turns;
-    // static final int X = 1;
-    // static final int Y = 2;
+    private static int turns;
+
     public static final int ROWS = 3, COLS = 3; // number of rows and columns
     // Instance Variables; board with 3 columns, 3 rows
     private static char[][] board = new char[3][3];
 
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        // Two players: X and O
-        char player1 = 'X', player2 = 'O';
-        // Initialize turn
-        int turns = 1;
+    // Constructor
+    public TicTacToeClass() {
+        board = new char[3][3];
+        turns = 0;
 
-        System.out.println("Let's Play Tic-Tac-Toe!");
-        // Call the board and Display it
-        initBoard();
-        displayBoard();
+        for (int r = 0; r < 3; r++)
+            for (int c = 0; c < 3; c++)
+                board[r][c] = ' ';
 
-        // Run this while there is not a winner and board is not full; "while playing"
-        while (!(isWinner('X') || isWinner('O') || isFull())) {
-
-            System.out.println("Turn #" + turns + ": ");
-            if (turns % 2 != 0) {
-                playerChoice(player1);
-                turns++;
-            } else {
-                playerChoice(player2);
-                turns++;
-            }
-
-            displayBoard(); // Display updated board after each turn
-
-            // check for game end
-            if (isWinner(player1)) {
-                System.out.println("'" + player1 + "' wins the game!");
-                break;
-            } else if (isWinner(player2)) {
-                System.out.println("'" + player2 + "' wins the game!");
-                break;
-                // If game has more than 9 turns, it is a tie
-            } else if (turns > 9) {
-                System.out.println("This game is a tie.");
-                break;
-            }
-
-        }
     }
 
     // Accessor Methods
-    private static void playerChoice(char p) {
-        // Take user input to find rows and columns
-        Scanner keyboard = new Scanner(System.in);
-        int r, c;
-        System.out.print("'" + p + "', choose your location (row, column): ");
-        r = keyboard.nextInt();
-        c = keyboard.nextInt();
-
-        // If the spot is already taken/not valid, try again.
-        // Currently exits game when invalid. Need to fix incrementing turns
-        if (isValid(r, c) == false) {
-            System.out.println("That is not a valid location. Try again.");
-        } else if (playerAt(r, c) != ' ') {
-            System.out.println("That location is already full. Try again.");
-        }
-        // Shows board after move
-        board[r][c] = p;
-
-        playMove(p, r, c);
-
-    }
 
     // initialize the board
     private static void initBoard() {
@@ -85,6 +31,8 @@ public class TicTacToeClass {
 
     // Displays the board
     public static void displayBoard() {
+        System.out.println("Turn #" + turns + ": ");
+
         System.out.println("  0  " + board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
         System.out.println("    --+-+--");
         System.out.println("  1  " + board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
@@ -121,27 +69,20 @@ public class TicTacToeClass {
 
     // returns true if nine turns have been played and false otherwise
     public static boolean isFull() {
-        return false;
-        // for (int r = 0; r < 3; r++) {
-        // for (int c = 0; c < 3; c++) {
-        // if (board[r][c] == EMPTY) {
-        // return false;
-        // }
-        // }
-        // }
-        // return true;
+        if (turns == 9) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // Returns true if all nine spaces are filled AND neither X nor O has won
     public boolean isCat() {
-        for (int r = 0; r < ROWS; ++r) {
-            for (int c = 0; c < COLS; ++c) {
-                if (board[r][c] == EMPTY) {
-                    return false; // an empty cell found, not draw, exit
-                }
-            }
+        if ((turns == 9) && !isWinner('X') && !isWinner('O')) {
+            return true;
+        } else {
+            return false;
         }
-        return true;
     }
 
     // Returns if space is valid
@@ -167,11 +108,8 @@ public class TicTacToeClass {
 
     // Modifiers
     public static void playMove(char p, int r, int c) {
-        // switch turns
-        if (p == 'X')
-            p = 'O';
-        else
-            p = 'X';
+        board[r][c] = p;
+        turns++; // increment number of turns each time
 
     }
 }
